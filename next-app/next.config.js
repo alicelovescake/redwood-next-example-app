df8a63810240947873b12e00e53a76a0a7ac9dc5
Alice Zhao <azhao991@gmail.com>
2022-04-17 15:23:20 -0700

    feat: add next app

---
+++ next-app/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    // Experimental monorepo support
+    // @link {https://github.com/vercel/next.js/pull/22867|OriginalPR}
+    // @link {https://github.com/vercel/next.js/discussions/26420|Discussion}
+    externalDir: true,
+  },
+}
+
+module.exports = nextConfig
---
+++ next-app/pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app'
+import { NominationsProvider } from '../context/NominationsContext'
+
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NominationsProvider>
+      <Component {...pageProps} />
+    </NominationsProvider>
+  )
+}
+
+export default MyApp
