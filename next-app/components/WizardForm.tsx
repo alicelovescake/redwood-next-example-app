b63f2c753c27a880bdb4895b9d9977deba787b7c
Alice Zhao <azhao991@gmail.com>
2022-05-04 14:15:27 -0700

    fix: add style to wizard form

--- next-app/components/WizardForm.tsx
+++ next-app/components/WizardForm.tsx
@@ -3,58 +3,74 @@ import {
   FormError,
   FieldError,
   Label,
-  TextField,
   Controller,
   useForm,
 } from '@redwoodjs/forms'
 
-import { Stack, Typography, Button } from '@mui/material'
+import { Stack, Typography, Button, Box, TextField } from '@mui/material'
 import * as wizards from '../graphql-client/wizards'
 import { CreateWizardInput } from 'types/graphql'
 
 export const WizardForm = () => {
   const formMethods = useForm()
   const onSubmit = async (input: CreateWizardInput) => {
+    console.log(input)
+    return
     await wizards.create(input)
   }
 
   return (
-    <Form formMethods={formMethods}>
-      <FormError />
-      <Stack spacing={2}>
-        <Typography variant="h5">
-          <Label name="firstName">First name</Label>
-        </Typography>
-
-        <TextField name="firstName" validation={{ required: true }} />
-
-        <FieldError name="firstName" />
-        <Typography variant="h5">
-          <Label
-            name="lastName"
-            className="rw-label"
-            errorClassName="rw-label rw-label-error"
-          >
-            Last name
-          </Label>
-        </Typography>
-        <TextField name="lastName" validation={{ required: true }} />
-        <FieldError name="lastName" />
-      </Stack>
-      <Controller
-        name="Button"
-        render={() => (
-          <Button
-            variant="contained"
-            sx={{ my: 5 }}
-            onClick={formMethods.handleSubmit(({ firstName, lastName }) =>
-              onSubmit({ firstName, lastName })
+    <Box>
+      <Typography sx={{ mb: 5 }} variant="h5">
+        Register as a New Wizard!
+      </Typography>
+      <Form formMethods={formMethods}>
+        <FormError />
+        <Stack spacing={2}>
+          <Controller
+            name="firstName"
+            render={({ field: { onChange, value } }) => (
+              <TextField
+                variant="filled"
+                required
+                label="First name"
+                onChange={onChange}
+                value={value}
+              />
             )}
-          >
-            Save
-          </Button>
-        )}
-      />
-    </Form>
+          />
+          <FieldError name="firstName" />
+
+          <Controller
+            name="lastName"
+            render={({ field: { onChange, value } }) => (
+              <TextField
+                variant="filled"
+                required
+                label="Last name"
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+          <FieldError name="lastName" />
+        </Stack>
+
+        <Controller
+          name="Button"
+          render={() => (
+            <Button
+              variant="contained"
+              sx={{ my: 5 }}
+              onClick={formMethods.handleSubmit(({ firstName, lastName }) =>
+                onSubmit({ firstName, lastName })
+              )}
+            >
+              Enroll
+            </Button>
+          )}
+        />
+      </Form>
+    </Box>
   )
 }
