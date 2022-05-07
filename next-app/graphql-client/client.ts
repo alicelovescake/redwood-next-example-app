70754d34dd10b23c7b0fcfe8a5663216ba1ae5ca
Alice Zhao <azhao991@gmail.com>
2022-05-06 18:43:53 -0700

    update api url

--- next-app/graphql-client/client.ts
+++ next-app/graphql-client/client.ts
@@ -1,5 +1,8 @@
 import { createClient } from 'urql'
 
 export const client = createClient({
-  url: 'http://localhost:8911/graphql'
+  url:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8911/graphql'
+      : 'https://redwood-next-example-app.vercel.app/api/graphql',
 })
