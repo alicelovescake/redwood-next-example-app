ed734ef3d44f3383eac75f3cfab9f869b4d1afb0
Alice Zhao <azhao991@gmail.com>
2022-05-18 10:00:40 -0700

    feat: add signIn page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -14,6 +14,7 @@ import ClassroomsLayout from 'src/layouts/ClassroomsLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/signin" page={SigninPage} name="signin" />
       <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/" page={HomePage} name="home" />
       <Set wrap={WizardsLayout}>
---
+++ web/src/components/SignoutBtn/SignoutBtn.tsx
@@ -0,0 +1,10 @@
+const SignoutBtn = () => {
+  return (
+    <div>
+      <h2>{'SignoutBtn'}</h2>
+      <p>{'Find me in ./web/src/components/SignoutBtn/SignoutBtn.tsx'}</p>
+    </div>
+  )
+}
+
+export default SignoutBtn
