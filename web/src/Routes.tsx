d8e5e2ca248c21051a7e9bebdc00d8ae0a5e2f32
Alice Zhao <azhao991@gmail.com>
2022-05-18 09:42:37 -0700

    feat: add sign up page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -14,6 +14,7 @@ import ClassroomsLayout from 'src/layouts/ClassroomsLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/" page={HomePage} name="home" />
       <Set wrap={WizardsLayout}>
         <Route path="/wizards/new" page={WizardNewWizardPage} name="newWizard" />
